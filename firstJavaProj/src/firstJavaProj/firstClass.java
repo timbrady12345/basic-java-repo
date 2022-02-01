fd48b4a620c81428daede228c8e87eb4de3880d6
tim brady <timbrady@uri.edu>
2022-02-01 18:47:00 -0500

    first commit woo!

---
+++ firstJavaProj/src/firstJavaProj/firstClass.java
@@ -0,0 +1,10 @@
+package firstJavaProj;
+
+public class firstClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ firstJavaProj/src/module-info.java
@@ -0,0 +1,2 @@
+module firstJavaProj {
+}
